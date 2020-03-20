e7ff360dcb64c20e4bdd7f1cdc070f5abbfa7327
krystal chen <krystal.chen@pandarg.com>
2020-03-20 09:59:14 -0700

    Field Added

---
+++ Fields/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public readonly List<OrderInfo> Orders = new List<OrderInfo>();  // always initialize List as an empty list 
+
+
+        public Customer(int inputId) // creating Customer constructor
+        {
+            this.Id = inputId;
+        }
+        public Customer(int inputId, string inputName) // creating Customer constructor
+            : this(inputId)
+        {
+            this.Name = inputName;
+        }
+
+        public void Promote() // creating a method called Promote()
+        {
+            // if I incidentally re-initialize or initialize the Orders here, it will cause bug when I invoke Customer and Promote
+            // Orders = new List<OrderInfo>();
+            //...
+        }
+
+    }
+}
---
+++ Fields/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fields
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer(1);
+
+            customer.Orders.Add(new OrderInfo());
+            customer.Orders.Add(new OrderInfo());
+        
+
+            customer.Promote();  // if I invoke the Promote() here, then it will reinitize the new Order() List
+            // as inside the Promote() method has re-initialize Orders = new List<Orders>()
+            // The solution will be to use readonly modifier, this way THE CODE know "Orders" only can invoke ONCE. 
+            // And gives an error in output
+
+            Console.WriteLine("Counting customer orders: " +  customer.Orders.Count);
+
+
+        }
+    }
+}
