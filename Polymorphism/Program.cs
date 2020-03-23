d56943d6c1d3c3f9893ba5c08763516ec2be5164
krystal chen <krystal.chen@pandarg.com>
2020-03-23 11:56:23 -0700

    Interfaces

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encoder = new VideoEncoder();
+            encoder.RegisterNotificationChannel(new MailNotificationChannel());
+            encoder.RegisterNotificationChannel(new SmsNotificationChannel());
+            encoder.Encode(new Video());
+        }
+    }
+}
