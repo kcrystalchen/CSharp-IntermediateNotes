4af500b1abd947b4372cc6a0a835a02de5e49ed6
krystal chen <krystal.chen@pandarg.com>
2020-03-24 09:59:59 -0700

    Design A Stack Over Flow Post

---
+++ DesignAStackOverFlowPost/Post.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DesignAStackOverFlowPost
+{
+    // 1. design a class called "Post" - property title, description and the date/time
+    // 2. up-vote or down-vote a post (up-vote and down-vote methods)
+    // 3. displaying current vote value (console.writeline current vote value)
+    // 4. Should not give ability to set the Vote property from the outside (vote are private)
+    public class Post
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateTime DateTime { get; set; }
+        private int CurrentVote { get; set; }
+
+
+        public int TotalVote()
+        {
+            Console.WriteLine("Total Vote Value " + CurrentVote);
+            return CurrentVote;
+        }
+
+        public int UpVote()
+        {
+            CurrentVote += 1;
+            return CurrentVote;
+        }
+        
+        public int downVote()
+        {
+            CurrentVote -= 1;
+            return CurrentVote;
+        }
+    } 
+}
---
+++ DesignAStackOverFlowPost/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignAStackOverFlowPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post = new Post();
+            post.title = "levelUp";
+            post.description = "Stack-over Flow post";
+            post.DateTime = DateTime.Now;
+
+
+            for(var x = 0; x < 10; x += 1)
+            {
+                post.UpVote();
+               
+            }
+
+             post.downVote();
+             post.TotalVote();
+
+
+
+             Console.WriteLine("The total Vote is " + post.TotalVote());
+        }
+    }
+}
