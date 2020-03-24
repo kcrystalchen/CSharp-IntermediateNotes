9cad6f01cb482a332c411003bdb9b0d68357907c
krystal chen <krystal.chen@pandarg.com>
2020-03-24 12:57:48 -0700

    Design a Database connection

---
+++ DesignADatabaseConnection/OracleConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignADatabaseConnection
+{
+    public class OracleConnection : DbConnection // OracleConnection will overwrite to the abstract base class (Polymorphism behavior)
+    {
+        public OracleConnection(string ConnectionString) : base(ConnectionString)
+        {
+            // because DbConnection passed in connectionString, so OracleConnection needs a constructor to pass in string as well
+        }
+
+        public override void ClosingConnection()
+        {
+            Console.WriteLine("Closing Oracle Connection");
+        }
+
+        public override void OpeningConnection()
+        {
+            Console.WriteLine("Opening Oracle Connection");
+        }
+    }
+}
---
+++ DesignADatabaseConnection/SqlConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DesignADatabaseConnection
+{
+    public class SqlConnection : DbConnection // SqlConnection will overwrite to the abstract base class (Polymorphism behavior)
+    {
+        public SqlConnection(string ConnectionString) : base(ConnectionString)
+        {
+            // because DbConnection passed in connectionString, so SqlConnection needs a constructor to pass in string as well
+
+        }
+
+        public override void ClosingConnection()
+        {
+            Console.WriteLine("Closing Sql Connection");
+        }
+
+        public override void OpeningConnection()
+        {
+            Console.WriteLine("Opening Sql Connection");
+        }
+    }
+}
