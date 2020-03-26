bd550e0d3fdd02798f46b97f22e3a70b5edc2d46
krystal chen <krystal.chen@pandarg.com>
2020-03-26 14:27:46 -0700

    Delaget

---
+++ CSharpAdvancedNotes/Delegates/Delegates/PhotoProcess.cs
@@ -0,0 +1,24 @@
+﻿namespace Delegates
+{
+    public class PhotoProcess
+    {
+        // delegate is a pointer for the method
+        public delegate void PhotoFilterHandler(Photo photo); // delegate method
+
+
+        public void Process(string path, PhotoFilterHandler photofilterHandler0)
+        {
+            Photo photo = Photo.Load(path);  // invoke the Photo.Load method passed in path
+
+            photofilterHandler0(photo);
+
+            //PhotoFilters photoFilters = new PhotoFilters();
+            //photoFilters.ApplyBrightness(photo);
+            //photoFilters.ApplyContrast(photo);
+            //photoFilters.Resize(photo);
+
+            photo.Save();
+             
+        }
+    }
+}
---
+++ CSharpAdvancedNotes/Delegates/Delegates/PhotoProcessorAction.cs
@@ -0,0 +1,22 @@
+﻿using System;using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+   
+    class PhotoProcessorAction
+    {
+        // this class is using system Action<>, it does not return anything
+        // system Func<> returning something 
+
+        public void Process(string path, Action<Photo> photoFilterHandler)
+        {
+            Photo photo = Photo.Load(path);
+            photoFilterHandler(photo);
+            photo.Save();
+        }
+
+    }
+}
