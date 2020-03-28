28734a01a3aa8b69947a398d585f923ec8f95c01
krystal chen <krystal.chen@pandarg.com>
2020-03-28 11:21:46 -0700

    EventAndDelegates

---
+++ CSharpAdvancedNotes/Delegates/EventsAndDelegets/MailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EventsAndDelegets
+{
+    // this mail server needs to subscribe the videoEncodedEvent's VideoEncoded
+    public class MailService
+    {
+        // onVideoEncoded is the handler in the MailService
+        public void OnVideoEncoded(object source, VideoEventArgs e)  //to pass the data from Video when the event happens
+        {
+            
+            Console.WriteLine("From MailService class..." + e.Video.Title);
+        }
+    }
+}
---
+++ CSharpAdvancedNotes/Delegates/EventsAndDelegets/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Video video = new Video() { Title = "Video 1"};
+
+
+            VideoEncoder videoEncoder = new VideoEncoder(); // publisher
+
+            MailService mailService = new MailService(); // subscriber
+            MessageServer messageServer = new MessageServer(); // another subscriber
+
+
+
+            // before called the Encode method in the VideoEncoder class 
+            // we need to setup the subscription from the Event - VideoEncoded Event (it was define from delegate VideoEncodedEventHandler)
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+            videoEncoder.VideoEncoded += messageServer.OnVideoEncoder; // this is not a method call, it is a reference or the pointer
+
+
+
+            // then call the encode method, otherwise subscriber wont be notified the event
+            videoEncoder.Encode(video);
+
+
+
+        }
+    }
+}
